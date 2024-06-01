d4a11896f97c9898a9cecb8e3621947b3d112dfb
bnaaedre <bnaaedre@myseneca.ca>
2024-06-01 00:01:44 +0000

    added lab1 files

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Define the object called name
+name = "Isaac"
+
+# Define the object called age
+age = 72
+
+# Print the statement with name and age
+print(f"{name} is {age} years old!")
---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Define integer objects x, y, and z with the specified values
+x = 10
+y = 2
+z = 5
+
+# Calculate the result and print it
+result = x + y * z
+print(f"{x} + {y} * {z} = {result}")
